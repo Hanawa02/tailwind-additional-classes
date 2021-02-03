df79a4cd339effdb7cce0d8656752f2c0937e23b
Laura Caroline <laura.c.r.melo@gmail.com>
2021-02-03 19:34:09 +0100

    Improve Tests

--- test/test.js
+++ test/test.js
@@ -1,7 +1,13 @@
+var fs = require("fs");
 const additionalClasses = require("../main.js");
 
 additionalClasses.pluginFunction({
   addUtilities: (item) => {
-    console.log(item);
+    fs.writeFile("./test/elements.json", JSON.stringify(item), function (err) {
+      if (err) throw err;
+    });
+    console.log(
+      "Test completed, see elements.json file to see the elements generated!"
+    );
   },
 });
